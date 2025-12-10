49963c11bbbe5f1b3b550d5c16c6327e62261a1b
Yusif Aliyev <114253515+YusifAliyevPro@users.noreply.github.com>
2025-12-11 02:28:03 +0400

    feat: wrap HybridMethodsPresentation in Suspense for improved loading handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import HybridMethodsPresentation from "@/components/presentation";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <NuqsAdapter>
-      <HybridMethodsPresentation />
+      <Suspense>
+        <HybridMethodsPresentation />
+      </Suspense>
     </NuqsAdapter>
   );
 }
